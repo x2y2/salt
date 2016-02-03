4c3c5397c2c93939e059e2ae27ced7bbffcb9eef
wangpei <wangpei456@126.com>
2016-02-03 16:22:11 +0800

    修改了ftp上传部分

--- _modules/apps.py
+++ _modules/apps.py
@@ -13,6 +13,16 @@ import json
 import ftplib
 from pwd import getpwnam
 
+def __ftp():
+    ftp_host = '172.17.192.32'
+    ftp_port = 21
+    ftp_user = 'night'
+    ftp_password = 'xxx'
+    ftp = ftplib.FTP()
+    ftp.connect(ftp_host,ftp_port)
+    ftp.login(ftp_user,ftp_password)
+    return ftp
+
 def version():
     url = 'http://localhost:8080/version.html'
     header = {'Accept':'application/json'} 
@@ -38,21 +48,15 @@ def status(url='http://localhost:8080/checkpreload.html'):
 def upload(appname,version):
     localpath = '/data/{0}/target'.format(appname)
     remotepath = '/production/{0}/{1}'.format(appname,version)
-    ftp_host = '172.17.192.32'
-    ftp_port = 21
-    ftp_user = 'night'
-    ftp_password = 'xxx'
-    buffersize = 1024
-    ftp = ftplib.FTP()
-    ftp.connect(ftp_host,ftp_port)
-    ftp.login(ftp_user,ftp_password)
+    ftp = __ftp()
     for key in range(1,len(remotepath.split('/'))):
         try:
             ftp.cwd(remotepath.split('/')[key])
         except:
             ftp.mkd(remotepath.split('/')[key])
             ftp.cwd(remotepath.split('/')[key])
-
+    
+    buffersize = 1024
     file_handler = open(os.path.join(localpath,appname + '.war'),'rb')
     try:
         ftp.storbinary('STOR ' + appname + '.war',file_handler,buffersize)
@@ -95,21 +99,15 @@ def deploy(appname,version):
     os.chdir(os.path.join(app_path,appname))
     os.system('rm -rf {0}/*'.format(os.path.join(app_path,appname)))
     #download .war from ftp
-    ftp_path = os.path.join('production',appname,str(version))
-    ftp_host = '172.17.192.32'
-    ftp_port = 21
-    ftp_user = 'night'
-    ftp_password = 'xxx'
-    buffersize = 1024
-    ftp = ftplib.FTP()
-    ftp.connect(ftp_host,ftp_port)
-    ftp.login(ftp_user,ftp_password)
+    ftp_path = '/production/{0}/{1}'.format(appname,version)
+    ftp = __ftp()
     try:
         ftp.cwd(ftp_path)
     except Exception,e:
         return e
     file_handler = open(appname + '.war','wb')
     os.chdir(os.path.join(app_path,appname))
+    buffersize = 1024
     try:
         ftp.retrbinary('RETR ' + appname + '.war',file_handler.write,buffersize)
     except Exception,e:
