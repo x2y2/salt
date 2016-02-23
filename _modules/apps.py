d3190e54d399f6665f31077eb8d9b664c30c18c1
wangpei <wangpei456@126.com>
2016-02-23 17:18:14 +0800

    应用模块优化

--- _modules/apps.py
+++ _modules/apps.py
@@ -48,8 +48,13 @@ def status(url='http://localhost:8080/checkpreload.html'):
 
 def upload(*args):
     arg = args[:]
-    localpath = '/data/{0}/target'.format(arg[0],arg[1])
-    remotepath = '/production/{0}/{1}'.format(arg[0],arg[1])
+    if len(arg) < 2:
+        return '2 arguments at least'
+    appname = arg[0]
+    version = arg[1]
+    localpath = arg[2]
+    #localpath = '/data/{0}/target'.format(arg[0],arg[1])
+    remotepath = '/production/{0}/{1}'.format(appname,version)
     ftp = __ftp()
     for key in range(1,len(remotepath.split('/'))):
         try:
@@ -59,29 +64,37 @@ def upload(*args):
             ftp.cwd(remotepath.split('/')[key])
     
     buffersize = 1024
-    file_handler = open(os.path.join(localpath,arg[0] + '.war'),'rb')
+    file_handler = open(localpath,'rb')
     try:
-        ftp.storbinary('STOR ' + arg[0] + '.war',file_handler,buffersize)
+        ftp.storbinary('STOR ' + appname + '.war',file_handler,buffersize)
     except ftplib.error_perm:
         return 'permission deny'
-    ret = '{0} upload successfully'.format(arg[0] + '.war')
+    files = ftp.nlst(os.path.join('/production',appname,version))
+    if files[0].split('/')[-1] == '{0}.war'.format(appname):
+        ret = '{0} upload successfully'.format(appname + '.war')
+    else:
+        ret = '{0} upload failed'.format(appname + '.war')
     file_handler.close()
     ftp.close()
     return ret
 
 def __download(app_path,*args):
     arg = args[:]
-    ftp_path = '/production/{0}/{1}'.format(arg[0],arg[1])
+    if len(arg) < 2:
+        return '2 arguments at least'
+    appname = arg[0]
+    version = arg[1]
+    ftp_path = '/production/{0}/{1}'.format(appname,version)
     ftp = __ftp()
     try:
         ftp.cwd(ftp_path)
     except Exception,e:
         return e
-    file_handler = open(arg[0] + '.war','wb')
-    os.chdir(os.path.join(app_path,arg[0]))
+    file_handler = open(appname + '.war','wb')
+    #remote_file = os.path.join(app_path,appname,appname + '.war')
     buffersize = 1024
     try:
-        ftp.retrbinary('RETR ' + arg[0] + '.war',file_handler.write,buffersize)
+        ftp.retrbinary('RETR ' + appname + '.war',file_handler.write,buffersize)
     except Exception,e:
         return e
     file_handler.close()
@@ -91,28 +104,34 @@ def __download(app_path,*args):
 def pull_code(*args):
     ret = []
     arg = args[:]
-    if len(arg) < 2:
-        return '2 arguments at least'
+    if len(arg) < 3:
+        return '3 arguments at least'
     match = re.search('[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}',arg[0])
     if match:
         url = arg[0]
-        path = '/data/{0}'.format(arg[1])
+        appname = arg[1]
+        version = arg[2]
+        branch = arg[3]
+        path = '/data/{0}'.format(appname)
     else:
         url = None
-        path = '/data/{0}'.format(arg[0])
-
+        appname = arg[0]
+        version = arg[1]
+        branch = arg[2]
+        path = '/data/{0}'.format(appname)
+    
     if not os.path.exists(path):
         if url:
             os.chdir(path.split('/')[1])
  
-            cmd = 'git clone {0}'.format(arg[0])
+            cmd = 'git clone {0}'.format(appname)
             (status,output) = commands.getstatusoutput(cmd)
             ret.append(output)
             try:
                 os.chdir(path)
             except:
                 return '{0} is not exist'.format(path)
-            cmd = 'git checkout {0}'.format(arg[3])
+            cmd = 'git checkout {0}'.format(branch)
             (status,output) = commands.getstatusoutput(cmd)
             ret.append(output)
 
@@ -126,15 +145,9 @@ def pull_code(*args):
         cmd = 'git pull'
         (status,output) = commands.getstatusoutput(cmd)
         ret.append(output)
-
-        if url:
-            cmd = 'git checkout {0}'.format(arg[3])
-            (status,output) = commands.getstatusoutput(cmd)
-            ret.append(output)
-        else:
-            cmd = 'git checkout {0}'.format(arg[2])
-            (status,output) = commands.getstatusoutput(cmd)
-            ret.append(output)
+        cmd = 'git checkout {0}'.format(branch)
+        (status,output) = commands.getstatusoutput(cmd)
+        ret.append(output)
         cmd = 'git pull'
         (status,output) = commands.getstatusoutput(cmd)
         ret.append(output)
@@ -146,17 +159,12 @@ def pull_code(*args):
         return ret
 
     (status,output) = commands.getstatusoutput('git log -1')
-    version = output.split('\n')[0][-4:]
+    ver = output.split('\n')[0][-4:]
 
-    if not url:
-        ver = arg[1]
+    if version != ver:
+        ret.append('{0} is not match {1}'.format(ver,version))
     else:
-        ver = arg[2]
-
-    if ver != version:
-        ret.append('{0} is not match {1}'.format(version,ver))
-    else:
-        ret.append('pull code completed,version is {0}'.format(version))
+        ret.append('pull code completed,version is {0}'.format(ver))
 
     return ret
 
@@ -164,9 +172,11 @@ def pull_code(*args):
 def make_package(*args):
     ret = []
     arg = args[:]
-    if len(arg) < 1:
-        return '1 arguments at least'
-    path = '/data/{0}'.format(arg[0])
+    if len(arg) < 2:
+        return '2 arguments at least'
+    appname = arg[0]
+    config_file = arg[1]
+    path = '/data/{0}'.format(appname)
     #update config file
     autoconfig_path = '/data/autoconfig-release'
     os.chdir(autoconfig_path)
@@ -175,10 +185,10 @@ def make_package(*args):
     ret.append(output)
 
     os.chdir(path)
-    autoconfig_path = '/data/autoconfig-release/{0}/{0}/product.properties'.format(arg[0])
+    #config_file = '/data/autoconfig-release/{0}/{0}/product.properties'.format(appname)
     cmd = 'nohup /tools/maven/bin/mvn clean package \
                 -Dautoconfig.userProperties={0} \
-                -Dmaven.test.skip=true'.format(autoconfig_path)
+                -Dmaven.test.skip=true'.format(config_file)
     (status,output) = commands.getstatusoutput(cmd)
     ret.append(output)
     #files = '/data/{0}/web/target/{0}.war'.format(arg[0])
@@ -219,49 +229,52 @@ def deploy(*args):
     arg = args[:]
     if len(arg) < 2:
         return '2 arguments at least'
+    appname = arg[0]
+    version = arg[1]
     app_path = '/tools/apps'
-    if os.path.exists(os.path.join(app_path,arg[0])):
+    if os.path.exists(os.path.join(app_path,appname)):
         #clean app
-        os.chdir(os.path.join(app_path,arg[0]))
-        cmd = 'rm -rf {0}/*'.format(os.path.join(app_path,arg[0]))
+        os.chdir(os.path.join(app_path,appname))
+        cmd = 'rm -rf {0}/*'.format(os.path.join(app_path,appname))
         (status,output) = commands.getstatusoutput(cmd)
         ret.append(output)
     else:
-        os.makedirs(os.path.join(app_path,arg[0]))
+        os.makedirs(os.path.join(app_path,appname))
+        os.chdir(os.path.join(app_path,appname))
     #download .war from ftp
     ftp = __ftp()
-    files = ftp.nlst(os.path.join('/production',arg[0],arg[1]))
+    files = ftp.nlst(os.path.join('/production',appname,version))
     ftp.close()
     if not files:
-        ftp_file = os.path.join('ftp://xxx/production',arg[0],arg[1],arg[0]+'.war')
+        ftp_file = os.path.join('ftp://xxx/production',appname,version,appname+'.war')
         return '{0} is not exist'.format(ftp_file)
 
     try:
         __download(app_path,*args)
     except:
-        return 'download {0} error'.format(arg[0])
+        return 'download {0} error'.format(appname)
 
     #unpackage war
-    os.chdir(os.path.join(app_path,arg[0]))
-    cmd = 'unzip {0}'.format(arg[0] + '.war')
+    os.chdir(os.path.join(app_path,appname))
+    cmd = 'unzip {0}'.format(appname + '.war')
     try:
         (status,output) = commands.getstatusoutput(cmd)
         ret.append(output)
     except:
-        return '{0} is not found'.format(os.path.join(app_path,arg[0 + '.war']))
+        return '{0} is not found'.format(os.path.join(app_path,appname,appname + '.war'))
  
-    cmd = os.path.join(app_path,arg[0],arg[0] + '.war')
+    cmd = os.path.join(app_path,appname,appname + '.war')
     try:
         os.remove(cmd)
         ret.append('remove {0}'.format(cmd))
     except:
-        return '{0} is not exist'.format(os.path.join(app_path,arg[0],arg[0] + '.war'))
+        return '{0} is not exist'.format(os.path.join(app_path,appname,appname + '.war'))
 
     try:
-        os.system('chown -R bestpay.bestpay ' + os.path.join(app_path,arg[0]))
-        ret.append('deploy {0} successfully'.format(arg[0]))
+        os.system('chown -R bestpay.bestpay ' + os.path.join(app_path,appname))
+        ret.append('deploy {0} successfully'.format(appname))
     except:
-        ret.append('deploy {0} failed'.format(arg[0]))
+        ret.append('deploy {0} failed'.format(appname))
 
     return ret
 
@@ -270,18 +283,20 @@ def rollback(*args):
     arg = args[:]
     if len(arg) < 2:
         return '2 arguments at least'
+    appname = arg[0]
+    version = arg[1]
     app_path = '/tools/apps'
-    appbak_path = '/data/appbak/{0}/{1}'.format(arg[0],arg[1])
+    appbak_path = '/data/appbak/{0}/{1}'.format(appname,version)
     #clean app
     os.chdir(app_path)
-    os.system('rm -rf {0}'.format(arg[0]))
+    os.system('rm -rf {0}'.format(appname))
     if not os.path.exists(appbak_path):
         return '{0} is not found'.format(appbak_path)
     try:
-        shutil.copytree(os.path.join(appbak_path,arg[0]),os.path.join(app_path,arg[0]))
-        os.system('chown -R bestpay.bestpay {0}'.format(os.path.join(app_path,arg[0])))
-        ret = '{0} rollback successfully'.format(arg[0])
+        shutil.copytree(os.path.join(appbak_path,appname),os.path.join(app_path,appname))
+        os.system('chown -R bestpay.bestpay {0}'.format(os.path.join(app_path,appname)))
+        ret = '{0} rollback successfully'.format(appname)
     except:
-        ret = '{0} rollback failed'.format(arg[0])
+        ret = '{0} rollback failed'.format(appname)
 
     return ret
